8053c2ef5a8e8bb712408141ede2b7d10e9c7a75
kyriediculous <vergauwennico@gmail.com>
2023-06-15 05:28:04 +0200

    fix typo in path

--- src/mappings/registry.ts
+++ src/mappings/registry.ts
@@ -1,25 +1,25 @@
 import { Asset, Tenderizer } from '../types/schema'
-import { NewTenderizer } from '../types/Registry/registry'
+import { NewTenderizer } from '../types/Registry/Registry'
 import { ERC20 } from '../types/Registry/ERC20'
 import { BigDecimal } from '@graphprotocol/graph-ts'
 
 export function handleNewTenderizer(event: NewTenderizer): void {
-  let asset = Asset.load(event.params.asset.toHex())
-  if (asset == null) {
-    asset = new Asset(event.params.asset.toHex())
-    asset.tvl = BigDecimal.fromString('0')
-    asset.tenderizerCount = 0
-  }
+    let asset = Asset.load(event.params.asset.toHex())
+    if (asset == null) {
+        asset = new Asset(event.params.asset.toHex())
+        asset.tvl = BigDecimal.fromString('0')
+        asset.tenderizerCount = 0
+    }
 
-  asset.tenderizerCount = asset.tenderizerCount + 1
-  asset.save()
+    asset.tenderizerCount = asset.tenderizerCount + 1
+    asset.save()
 
-  let tenderizer = new Tenderizer(event.params.tenderizer.toHex())
-  tenderizer.asset = asset.id
-  tenderizer.name = ERC20.bind(event.params.tenderizer).name()
-  tenderizer.symbol = ERC20.bind(event.params.tenderizer).symbol()
-  tenderizer.validator = event.params.validator.toHex()
+    let tenderizer = new Tenderizer(event.params.tenderizer.toHex())
+    tenderizer.asset = asset.id
+    tenderizer.name = ERC20.bind(event.params.tenderizer).name()
+    tenderizer.symbol = ERC20.bind(event.params.tenderizer).symbol()
+    tenderizer.validator = event.params.validator.toHex()
 
-  tenderizer.tvl = BigDecimal.fromString('0')
-  tenderizer.save()
+    tenderizer.tvl = BigDecimal.fromString('0')
+    tenderizer.save()
 }

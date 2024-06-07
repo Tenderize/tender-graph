25174061815eff28d873959f0a6a450769b4ecee
kyriediculous <vergauwennico@gmail.com>
2024-06-07 03:10:01 +0200

    fix: null error in unlock transfer handler

--- packages/subgraph/src/mappings/unlocks.ts
+++ packages/subgraph/src/mappings/unlocks.ts
@@ -1,16 +1,24 @@
 
-import { Unlock } from "../types/schema";
+import { Unlock, User } from "../types/schema";
 import { Transfer as UnlockTransferEvent } from "../types/Unlocks/Unlocks";
-import { decodeTokenId } from "./helpers";
+import { ADDRESS_ZERO, decodeTokenId } from "./helpers";
 
 export function handleUnlockTransfer(event: UnlockTransferEvent): void {
+    if (event.params.from === ADDRESS_ZERO) return;
 
     const decoded = decodeTokenId(event.params.id)
     const idAsString = decoded.id.toString()
-    let unlockID = decoded.tenderizer.toHex().concat('0'.repeat(24 - idAsString.length).concat(idAsString))
+    let unlockID = decoded.tenderizer.concat('0'.repeat(24 - idAsString.length).concat(idAsString))
 
     let u = Unlock.load(unlockID);
     if (u == null) return;
-    u.user = event.params.to.toHex();
+    let receiver = event.params.to.toHex();
+    let user = User.load(receiver)
+    if (user == null) {
+        user = new User(receiver)
+        user.save()
+    }
+
+    u.user = receiver;
     u.save();
 }
